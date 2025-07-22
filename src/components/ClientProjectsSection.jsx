7856acd08fd5208ab15a3797d0fb9ac9b613437f
Felix  Osei Danso <felix.osei@amalitech.com>
2025-07-22 18:01:32 +0000

    udpating my profile

--- src/components/ClientProjectsSection.jsx
+++ src/components/ClientProjectsSection.jsx
@@ -23,35 +23,35 @@ const ClientProjectsSection = () => {
       duration: "4 months"
     },
     {
-        id: 4,
-        title: "Kolula",
-        technologies: ["React", "Node", "PostgreSQL", "RduxToolkit" ],
-        duration: "13 months"
-      },
-      {
-        id: 5,
-        title: "Imperial shuttle",
-        technologies: ["React", "Node", "PostgreSQL", "RduxToolkit" ],
-        duration: "10 months"
-      },
-      {
-        id: 6,
-        title: "Komet",
-        technologies: ["Nextjs", "Neon", "postgress db", "Geminia api" ],
-        duration: "1 months"
-      },
-      {
-        id: 7,
-        title: "safe Report",
-        technologies: ["Nextjs", "storyblok", "commence tools", "Magnolia" ],
-        duration: "1 months"
-      },
-      {
-        id: 8,
-        title: "PickNDrop",
-        technologies: ["React Native", "Django", "PostgreSQL", "RduxToolkit" ],
-        duration: "1 months"
-      },
+      id: 4,
+      title: "Kolula",
+      technologies: ["React", "Node", "PostgreSQL", "Redux Toolkit"],
+      duration: "13 months"
+    },
+    {
+      id: 5,
+      title: "Imperial shuttle",
+      technologies: ["React", "Node", "PostgreSQL", "Redux Toolkit"],
+      duration: "10 months"
+    },
+    {
+      id: 6,
+      title: "Komet",
+      technologies: ["Next.js", "Neon", "PostgreSQL", "Geminia API"],
+      duration: "1 month"
+    },
+    {
+      id: 7,
+      title: "Safe Report",
+      technologies: ["Next.js", "Storyblok", "Commence Tools", "Magnolia"],
+      duration: "1 month"
+    },
+    {
+      id: 8,
+      title: "PickNDrop",
+      technologies: ["React Native", "Django", "PostgreSQL", "Redux Toolkit"],
+      duration: "1 month"
+    }
   ];
 
   return (
@@ -61,7 +61,7 @@ const ClientProjectsSection = () => {
       </h2>
       <div className="max-w-4xl mx-auto px-4">
         <p className="text-center text-gray-300 mb-8">
-          I've had the privilege of working on several enterprise-level projects for prestigious clients. Due to confidentiality agreements, I cannot display the full details or screenshots of these projects. However, I can share some key information about my contributions:
+          I&apos;ve had the privilege of working on several enterprise-level projects for prestigious clients. Due to confidentiality agreements, I cannot display the full details or screenshots of these projects. However, I can share some key information about my contributions:
         </p>
         <div className="grid gap-8 md:grid-cols-2">
           {clientProjects.map((project) => (
